cb12c7d7dde94a6df1ea01c268c9ca0ce5d66f86
Ahmed walaaeldin <ahmedwalaaeldin1@gmail.com>
2025-01-25 04:26:40 +0200

    Headers buttons functionality

--- frontend/src/HomePage.jsx
+++ frontend/src/HomePage.jsx
@@ -1,17 +1,26 @@
-import LandingPage from './LandingPage/LandingPage'
-import ProjectsView from './ProjectsView/ProjectsView'
-import TeamView from './TeamView/TeamView'
-import ResourcesView from './ResoursesView/ResoursesView'
+import React from 'react';
+import LandingPage from './LandingPage/LandingPage';
+import ProjectsView from './ProjectsView/ProjectsView';
+import TeamView from './TeamView/TeamView';
+import ResourcesView from './ResourcesView/ResourcesView';
 
-function HomePage() {
+function HomePage({ homeRef, projectsRef, teamRef, resourcesRef }) {
   return (
     <>
-      <LandingPage />
-      <ProjectsView />
-      <TeamView />
-      <ResourcesView />
+      <div ref={homeRef}>
+        <LandingPage />
+      </div>
+      <div ref={projectsRef}>
+        <ProjectsView />
+      </div>
+      <div ref={teamRef}>
+        <TeamView />
+      </div>
+      <div ref={resourcesRef}>
+        <ResourcesView />
+      </div>
     </>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
